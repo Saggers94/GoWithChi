1d4d87d232b4607f3b1ac3c785cacf6d9d5e8e52
sagar.parmar <sagar.parmar@norimaconsulting.com>
2024-10-19 15:15:11 -0500

    chi router and middleware added

--- Orders-api/main.go
+++ Orders-api/main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"github.com/go-chi/chi/v5" 
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	// Because we have set this with Get method, It will only accept GET requests
+	router.Get("/hello", basicHandler) // We can just use the basicHandler function
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router, // We can just pass router because the chi router conforms to the http.Server handler
 	}
 	err := server.ListenAndServe()
 	if err != nil {
